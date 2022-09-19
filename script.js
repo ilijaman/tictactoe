6566fe30438d97cd4adaaeaf34c3da462d68e4b1
manojlovic <manojlovic@outlook.com>
2022-09-19 19:15:32 +1000

    initial const declared in js

---
+++ script.js
@@ -0,0 +1,18 @@
+const topLeftCell = document.querySelector('#t-left')
+const topMiddleCell = document.querySelector('#t-middle')
+const topRightCell = document.querySelector('#t-right')
+
+const middleLeftCell = document.querySelector('#m-left')
+const middleMiddleCell = document.querySelector('#m-middle')
+const middleRightCell = document.querySelector('#m-right')
+
+const bottomLeftCell = document.querySelector('b-left')
+const bottomMiddleCell = document.querySelector('b-middle')
+const bottomRightCell = document.querySelector('b-right')
+
+const restartButton = document.querySelector('#restart')
+
+const bartToken = document.querySelector('#bart-token')
+const homerToken = document.querySelector('#homer-token')
+
+
