27d536e39f262775bd356d8c3f72071097082022
manojlovic <manojlovic@outlook.com>
2022-09-20 10:32:58 +1000

    html each cell given a class instead of id

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-const topLeftCell = document.querySelector('#t-left')
-const topMiddleCell = document.querySelector('#t-middle')
-const topRightCell = document.querySelector('#t-right')
-
-const middleLeftCell = document.querySelector('#m-left')
-const middleMiddleCell = document.querySelector('#m-middle')
-const middleRightCell = document.querySelector('#m-right')
-
-const bottomLeftCell = document.querySelector('b-left')
-const bottomMiddleCell = document.querySelector('b-middle')
-const bottomRightCell = document.querySelector('b-right')
+const cells = document.querySelectorAll('.cell')
 
 const restartButton = document.querySelector('#restart')
 
-const bartToken = document.querySelector('#bart-token')
+const gameBoard = document.querySelectorAll('#Game-board')
+
+// const bart = document.createElement('img')
+// bart.src = 'bart-for-cell.png'
+// topLeftCell.appendChild(bart)
+// bart.className = 'bart-token'
+
+
+
+
 const homerToken = document.querySelector('#homer-token')
+console.log(bart)
+console.log(topLeftCell)
+
+
+
+const winningConditions = [ 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+]
+
+// everytime a cell is clicked - add X's 
+// if player one is X and two is O, first tile X, need to make sure the following click is an O and keeps switching 
+// make sure you can't click again on a cell that already has X/O
+
+
+
+
 
 
